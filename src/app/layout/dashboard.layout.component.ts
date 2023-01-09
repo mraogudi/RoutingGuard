5c3c3c2c105df70969e8cf9a0a486ea1451eca88
mraogudi <malligmrao514@gmail.com>
2023-01-09 10:26:47 +0530

    Added menu css

--- src/app/layout/dashboard.layout.component.ts
+++ src/app/layout/dashboard.layout.component.ts
@@ -1,17 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
-  template: `
-	<nav [ngClass] = "'parent-menu'">
-	  <ul>
-		 <li><a routerLink="/article" routerLinkActive="active" >Article</a></li>
-		 <li><a routerLink="/address" routerLinkActive="active">Address</a></li>
-	  </ul> 
-	</nav>  
-	<logout></logout>
-	<div [ngClass] = "'parent-container'">	
-	  <router-outlet></router-outlet>	
-	</div>
-  `
+  templateUrl: './dashboard.layout.component.html',
+  styleUrls: ['./dashboard.layout.component.css']
 })
 export class DashboardLayoutComponent { 
 }
